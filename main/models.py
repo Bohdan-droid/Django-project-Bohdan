d968ae32333ec8f77706c00afc151c88c09a79ea
Bohdan Novosad <bohdannovosad727@gmail.com>
2025-05-29 22:20:54 +0300

    Lab5

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    is_available = models.BooleanField(default=True)
+    color = models.CharField(max_length=50, choices=[
+        ('red', 'Червоний'),
+        ('blue', 'Синій'),
+        ('black', 'Чорний'),
+        ('white', 'Білий'),
+        ('green', 'Зелений')
+    ])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
